74ce9284f89285088f7c18f344132927562571d9
Avi <o.karimov@tuit.uz>
2023-08-08 21:31:22 +0500

    HMAC fix

--- task3/HMACGenerator.cs
+++ task3/HMACGenerator.cs
@@ -4,6 +4,8 @@ using Org.BouncyCastle.Crypto.Digests;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
 
 namespace task3
 {
@@ -11,6 +13,16 @@ namespace task3
     {
         static public string Hmac(string input)
         {
+            {
+            using (HMACSHA256 hmac = new HMACSHA256(key))
+            {
+                byte[] hashBytes = hmac.ComputeHash(data);
+                string hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                Console.WriteLine(hash);
+            }
+        
+
+        
             Sha3Digest sha3 = new Sha3Digest(256);
             byte[] inputbyte = Encoding.UTF8.GetBytes(input);
             sha3.BlockUpdate(inputbyte, 0, inputbyte.Length);
